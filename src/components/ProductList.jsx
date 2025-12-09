a7b0418a42eac35b16e9d1c71612dbd40f2ce352
Guanyu Lin <limkoaniun@gmail.com>
2025-12-09 22:51:02 +1100

    add product context

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,13 +1,16 @@
 import ProductCard from "./ProductCard.jsx";
+import {useProducts} from "../context/ProductContext.jsx";
 
-const ProductList = ({products}) => {
-    return (
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-            {products.map((product) => (
-                <ProductCard key={product.id} product={product}/>
-            ))}
-        </div>
-    )
+const ProductList = () => {
+    const {products, loading, error} = useProducts();
+
+    return (<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+        {loading && <p>loading..</p>}
+        {error && <div className="error">
+            {error}
+        </div>}
+        {products.map((product) => (<ProductCard key={product.id} product={product}/>))}
+    </div>)
 }
 
 export default ProductList;
---
+++ src/context/ProductContext.jsx
@@ -0,0 +1,36 @@
+import {createContext, useContext, useEffect, useState} from "react";
+
+const ProductContext = createContext();
+
+export function ProductProvider({children}) {
+    const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchProducts = async () => {
+            try {
+                const res = await fetch("/api/products")
+                if (!res.ok) throw new Error("Failed to fetch products")
+                const data = await res.json();
+                setProducts(data);
+            } catch (e) {
+                setError(e.message);
+            } finally {
+                setLoading(false)
+            }
+        }
+        fetchProducts();
+    }, []);
+
+    return (
+        <ProductContext.Provider value={{products, loading, error}}>
+            {children}
+        </ProductContext.Provider>
+    )
+}
+
+// custom hook
+export function useProducts() {
+    return useContext(ProductContext);
+}
