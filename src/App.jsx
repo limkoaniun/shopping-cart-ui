22e965808fe965cceadc56351d65bd08d25c606a
Guanyu Lin <limkoaniun@gmail.com>
2025-12-10 13:03:08 +1100

    add header and cart count

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import ProductList from "./components/ProductList.jsx";
+import Header from "./components/Header.jsx";
 
 const App = () => {
-    return (<div className="min-h-screen bg-gray-100 p-6">
-        <h1 className="text-3xl font-bold mb-6">🛒 Product Catalog</h1>
-        <ProductList/>
-    </div>)
+    return (
+        <>
+            <Header/>
+            <div className="min-h-screen bg-gray-100 p-6">
+                <h1 className="text-3xl font-bold mb-6">🛒 Product Catalog</h1>
+                <ProductList/>
+            </div>
+        </>
+    )
 }
 
 export default App;
